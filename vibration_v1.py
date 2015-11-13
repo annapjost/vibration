d38e1b4cdfb39c156ac8aef1b9e0020591ce1d69
Anna Jost <anna.paynetobin@gmail.com>
2015-11-13 14:51:35 -0500

    small changes to annotation.  still functional.

--- vibration_v1.py
+++ vibration_v1.py
@@ -9,20 +9,13 @@ from ij.gui import GenericDialog
 from os import path
 import math
 
-def getNoiseTolerance():
-    gd = GenericDialogPlus('set parameters')
-    gd.addStringField('noise tolerance: ', None)
-    gd.showDialog()
-    noise = gd.getNextString()
-    return noise
+def plotPoint(allpointslist, xory, point):
+    plot = Plot(xory + " position " + str(point), "timepoint", "position (nm)", [], [])
+    plot.addPoints(range(len(allpointslist[point])), allpointslist[point], Plot.LINE)
+    plot_window = plot.show()
 
-def dialog(title, field):
-    gd = GenericDialogPlus(title)
-    gd.addStringField(field, None)
-    gd.showDialog()
-    output = gd.getNextString()
-    return output
-
+    
+    
 def getResultsXCsYCs():
     '''grabs the XC and YC columns from the results table output from gaussfit_onspot plugin'''
     #grab the results table:
@@ -45,8 +38,6 @@ def isFrames(imp):
     """Determine whether the stack has >1 slices or >1 frames"""
     NSlices = imp.getNSlices()
     NFrames = imp.getNFrames()
-    print "number of slices:", NSlices
-    print "number of frames:", NFrames
     if NSlices == 1 and NFrames != 1:
         return True
     elif NSlices != 1 and NFrames == 1:
@@ -55,6 +46,7 @@ def isFrames(imp):
         print "stack dimension error!"
 
 def getCal(imp):
+    """Get pixel size calibration (in nm) from image unless it is uncalibrated"""
     imp = IJ.getImage()
     cal = imp.getCalibration()
     if cal.unit == 'pixel':
@@ -64,13 +56,14 @@ def getCal(imp):
         return scale            
         
 def findApproveMaxima(imp):
+    """set appropriate noise tolerance for Find Maxima by trial and error"""
     gd = GenericDialogPlus('noise tolerance')
     gd.addStringField('starting noise tolerance: ', None)
     gd.showDialog()
     noise = gd.getNextString()
     IJ.run("Find Maxima...", "noise=" + str(noise) + " output=[Point Selection] exclude")
     approved = False
-    while approved == False:
+    while True:
         gd = GenericDialog('confirm noise tolerance')
         gd.addMessage('are you happy with maxima?')
         gd.enableYesNoCancel()
@@ -79,7 +72,6 @@ def findApproveMaxima(imp):
             return noise
             approved = True
         elif gd.wasCanceled():
-            print "canceled"
             return False
         else:
             gd = GenericDialogPlus('enter new noise tolerance')
@@ -89,6 +81,8 @@ def findApproveMaxima(imp):
             IJ.run("Find Maxima...", "noise=" + str(noise) + " output=[Point Selection] exclude")
 
 def findAndFit(noise, scale):      #might not be a bad idea to have optional inputs here as tags...
+    """Given a noise tolerance for Find Maxima and a pixel size scale for GaussFit OnSpot, 
+    does a Gaussian fit for each maxima and reports coordinates in nm"""
     imp = IJ.getImage()
     stack = imp.getImageStack()
     IJ.run("Find Maxima...", "noise=" + str(noise) + " output=[Point Selection] exclude")
@@ -139,82 +133,82 @@ imp = IJ.getImage()
 
 scale = getCal(imp)
 if scale == False:
-    gd = GenericDialogPlus('set scale')
+    gd = GenericDialogPlus('enter calibration')
     gd.addStringField('pixel size (nm): ', None)
     gd.showDialog()
     scale = gd.getNextString()
 
 noise = findApproveMaxima(imp)
-xtimepoints, ytimepoints, numpoints = findAndFit(noise, scale)
-print numpoints
+if noise == False:
+    print "User clicked cancel!"
+else:
+    xtimepoints, ytimepoints, numpoints = findAndFit(noise, scale)
+    
 
-x = []
-y = []
+    x = []
+    y = []
 
-# plot all points
-for i in xtimepoints:
-    for j in range(numpoints):
-        x.append(i[j])
+    # plot all points
+    for i in xtimepoints:
+    	for j in range(numpoints):
+    		x.append(i[j])
+    
+    for k in ytimepoints:
+	    for l in range(numpoints):
+		    y.append(k[l])
 
-for k in ytimepoints:
-    for l in range(numpoints):
-        y.append(k[l])
-
-plot = Plot("summary", "xc", "yc", [], [])
-
-#plot.setColor(Color.BLACK)
-plot.addPoints(x, y, Plot.CIRCLE)
-plot_window = plot.show()
-
-# reorganize the lists:
-
-allpointsx = []
-
-for point in range(numpoints):
-    points = []
-    for time in range(len(xtimepoints)):
-        current = xtimepoints[time]
-        points.append(current[point])   
-    allpointsx.append(points)
-print allpointsx
-
-allpointsy = []
-
-for point in range(numpoints):
-    points = []
-    for time in range(len(ytimepoints)):
-        current = ytimepoints[time]
-        points.append(current[point])   
-    allpointsy.append(points)    
-print allpointsy
-
-def plotPoint(allpointslist, xory, point):
-    plot = Plot(xory + " position " + str(point), "timepoint", "position (nm)", [], [])
-    plot.addPoints(range(len(allpointslist[point])), allpointslist[point], Plot.LINE)
+    plot = Plot("summary", "xc", "yc", [], [])
+    
+    #plot.setColor(Color.BLACK)
+    plot.addPoints(x, y, Plot.CIRCLE)
     plot_window = plot.show()
 
-plotPoint(allpointsx, "x", 1)
-plotPoint(allpointsy, "y", 1)
+    # reorganize the lists:
+
+    allpointsx = []
+
+    for point in range(numpoints):
+    	points = []
+    	for time in range(len(xtimepoints)):
+    		current = xtimepoints[time]
+    		points.append(current[point])   
+    	allpointsx.append(points)
+    
+    
+    allpointsy = []
+
+    for point in range(numpoints):
+    	points = []
+    	for time in range(len(ytimepoints)):
+    		current = ytimepoints[time]
+    		points.append(current[point])   
+    	allpointsy.append(points)    
+
+    
+    plotPoint(allpointsx, "x", 1)
+    plotPoint(allpointsy, "y", 1)
+
+
 #now, allpoints is a list of points.  each entry contains every timepoint for that point.
 
 # do this a sort of crappy way for now - stdev in x and in y separately. 
-xstdevs = []
-ystdevs = [] 
+    xstdevs = []
+    ystdevs = [] 
 
-for point in range(numpoints):
-    std = stdev(allpointsx[point])
-    xstdevs.append(std)
-    
-for point in range(numpoints):
-    std = stdev(allpointsy[point])
-    ystdevs.append(std)
+    for point in range(numpoints):
+    	std = stdev(allpointsx[point])
+    	xstdevs.append(std)
+
+    for point in range(numpoints):
+    	std = stdev(allpointsy[point])
+    	ystdevs.append(std)
 
 
-avgxstdev = average(xstdevs)
-avgystdev = average(ystdevs)
+    avgxstdev = average(xstdevs)
+    avgystdev = average(ystdevs)
 
-print avgxstdev
-print avgystdev
+    print avgxstdev
+    print avgystdev
 
 
 #trying to make this work to measure distances from the centroid for each point over time...
